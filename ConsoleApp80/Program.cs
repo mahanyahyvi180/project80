16093702545dc2de0f07eb1779dbf1a2803f216d
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-10 10:36:20 +0330

    Add files via upload

---
+++ ConsoleApp80/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp80
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numberGrid = {
+
+                {1,2 },
+                {3,4 },
+                {5,6 }
+            };
+
+            Console.WriteLine(numberGrid[0, 1]);
+
+            Console.ReadLine();
+        }
+    }
+}
